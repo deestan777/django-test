675c7c45671f7812b0ad51b7c93915cb2dd55771
deestan <deestan@ymail.com>
2022-01-02 08:28:17 +0200

    admin panel and create task page

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('contacts', views.contacts, name='contacts'),
+    path('create', views.create, name='create')
 ]
